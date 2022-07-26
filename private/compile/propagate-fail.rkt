17fd5e62cd2cd1fee746a2d68f016131449d3cd3
gamburgm <33709622+gamburgm@users.noreply.github.com>
2022-07-25 22:29:01 -0400

    Propagate `failure` up as much as possible (#31)
    * basic implementation complete
    
    * tests wip
    
    * finished with testing

---
+++ private/compile/propagate-fail.rkt
@@ -0,0 +1,204 @@
+#lang racket/base
+
+(require syntax/parse
+         (for-template racket/base
+                       "../forms.rkt")
+         "utils.rkt"
+         "../syntax-classes.rkt")
+
+(provide propagate-fail/rel
+         propagate-fail/run)
+
+(define (propagate-fail/rel stx)
+  (syntax-parse stx #:literal-sets (mk-literals)
+    [(ir-rel (x ...) g)
+     #`(ir-rel (x ...) #,(propagate-fail/goal #'g))]))
+
+(define (propagate-fail/run stx)
+  (syntax-parse stx #:literal-sets (mk-literals)
+    [(run n (q ...) g)
+     #`(run n (q ...) #,(propagate-fail/goal #'g))]
+    [(run* (q ...) g)
+     #`(run* (q ...) #,(propagate-fail/goal #'g))]))
+
+(define HAS-RKT-TERM 'contains-rkt-term)
+
+(define (contains-rkt-term/cache g)
+  (or (syntax-property g HAS-RKT-TERM)
+      (contains-rkt-term? g)))
+
+(define (propagate-fail/conj g1 g2)
+  (let ([g1^ (propagate-fail/goal g1)]
+        [g2^ (propagate-fail/goal g2)])
+    (syntax-parse #`(conj #,g1^ #,g2^)
+      #:literal-sets (mk-literals)
+      [(conj (failure) _) #'(failure)]
+      [(conj _ (failure)) #:when (not (contains-rkt-term/cache g1^))
+       (if (contains-rkt-term/cache g1^)
+         (syntax-property this-syntax HAS-RKT-TERM #t)
+         #'(failure))]
+      ;; the syntax property prevents exponential blowup
+      ;; in left-associative conj-es containing a rkt-term
+      [_ this-syntax])))
+
+(define (propagate-fail/goal g)
+  (syntax-parse g #:literal-sets (mk-literals)
+    [(c:nullary-constraint) this-syntax]
+    [(c:unary-constraint t) this-syntax]
+    [(c:binary-constraint t1 t2) this-syntax]
+    [(conj g1 g2) (propagate-fail/conj #'g1 #'g2)]
+    [(disj g1 g2)
+     #`(disj #,(propagate-fail/goal #'g1)
+             #,(propagate-fail/goal #'g2))]
+    [(fresh (x ...) g)
+     #`(fresh (x ...)
+         #,(propagate-fail/goal #'g))]
+    [(#%rel-app n t ...) this-syntax]
+    [(apply-relation e t ...) this-syntax]))
+
+(module+ test
+  (require "./test/unit-test-progs.rkt"
+           "../forms.rkt"
+           rackunit
+           (for-syntax racket/base
+                       "./test/unit-test-progs.rkt"
+                       (submod "..")))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ()
+          (failure))))
+    (generate-prog
+      (ir-rel ()
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (conj
+            (== (#%lv-ref q) (#%term-datum 5))
+            (failure)))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (conj
+            (failure)
+            (== (#%lv-ref q) (#%term-datum 5))))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binders q p))
+          (conj
+            (conj
+              (== (#%lv-ref q) (#%term-datum 5))
+              (failure))
+            (== (#%lv-ref p) (#%term-datum 6))))))
+    (generate-prog
+      (ir-rel ((~binders q p))
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binders q p))
+          (conj
+            (conj
+              (failure)
+              (== (#%lv-ref q) (#%term-datum 5)))
+            (== (#%lv-ref p) (#%term-datum 6))))))
+    (generate-prog
+      (ir-rel ((~binders q p))
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binders q p))
+          (conj
+            (conj
+              (== (#%lv-ref q) (#%term-datum 5))
+              (== (#%lv-ref p) (#%term-datum 6)))
+            (failure)))))
+    (generate-prog
+      (ir-rel ((~binders q p))
+        (failure))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ()
+          (fresh ()
+            (failure)))))
+    (generate-prog
+      (ir-rel ()
+        (fresh ()
+          (failure)))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (fresh ((~binder x))
+            (conj
+              (== (#%lv-ref q) (#%lv-ref x))
+              (failure))))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (fresh ((~binder x))
+          (failure)))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (disj
+            (== (#%lv-ref q) (#%term-datum 5))
+            (failure)))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (disj
+          (== (#%lv-ref q) (#%term-datum 5))
+          (failure)))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binders q p))
+          (disj
+            (conj
+              (== (#%lv-ref q) (#%term-datum 5))
+              (failure))
+            (conj
+              (failure)
+              (== (#%lv-ref p) (#%term-datum 5)))))))
+    (generate-prog
+      (ir-rel ((~binders q p))
+        (disj
+          (failure)
+          (failure)))))
+
+  (progs-equal?
+    (propagate-fail/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (conj
+            (== (#%lv-ref q) (rkt-term 5))
+            (failure)))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (conj
+         (== (#%lv-ref q) (rkt-term 5))
+         (failure)))))
+
+  )
---
+++ private/compile/utils.rkt
@@ -0,0 +1,45 @@
+#lang racket/base
+
+(require syntax/parse
+         (for-template racket/base
+                       "../forms.rkt")
+         "../syntax-classes.rkt")
+
+(provide contains-rkt-term?)
+
+(define (contains-rkt-term? g)
+  (define (goal-contains? g)
+    (syntax-parse g
+      #:literal-sets (mk-literals)
+      [(c:nullary-constraint) #f]
+      [(c:unary-constraint t)
+       (term-contains? #'t)]
+      [(c:binary-constraint t1 t2)
+       (or (term-contains? #'t1)
+           (term-contains? #'t2))]
+      [(conj g1 g2)
+       (or (goal-contains? #'g1)
+           (goal-contains? #'g2))]
+      [(disj g1 g2)
+       (or (goal-contains? #'g1)
+           (goal-contains? #'g2))]
+      [(fresh (x ...) g) (goal-contains? #'g)]
+      [(#%rel-app n t ...)
+       (ormap term-contains? (syntax->list #'(t ...)))]
+      [(#%apply-relation e t ...)
+       (ormap term-contains? (syntax->list #'(t ...)))]))
+
+  (define (term-contains? t)
+    (syntax-parse t
+      #:literal-sets (mk-literals)
+      #:literals (quote cons)
+      [(#%term-datum _) #f]
+      [(quote _) #f]
+      [(#%lv-ref _) #f]
+      [(rkt-term _) #t]
+      [(cons t1 t2)
+       (or (contains-rkt-term? #'t1)
+           (contains-rkt-term? #'t2))]))
+
+  (goal-contains? g))
+
