72ee20ff7a6a51800637f319690e117e13351be8
Michael Ballantyne <michael.ballantyne@gmail.com>
2022-11-18 23:43:36 -0500

    factor out benchmarking code into common; reuse bits from faster-mk

--- bench/base/bench.rkt
+++ bench/base/bench.rkt
@@ -7,62 +7,6 @@
 (require "../utils.rkt")
 (require "four-fours.rkt")
 
-(define complex-countdown
-  '(((lambda (w) (w w))
-     (lambda (f)
-       (lambda (n)
-         ((lambda (id)
-            ((n (lambda (_)
-                  ((f f) (lambda (f)
-                           (lambda (x)
-                             (((n (lambda (g)
-                                    (lambda (h)
-                                      (h (g f)))))
-                               (lambda (u) x))
-                              id))))))
-             id))
-          (lambda (f) f)))))
-    (lambda (f) (lambda (x) (f (f x))))))
+(displayln "faster-minikanren")
 
-(define (logo-hard-program)
-  (run 9 (b q r) (logo (build-num 68) b q r) (>1o q)))
-
-(module+ main
-  (benchmark-suite "numbers"
-    ["logo-hard" (logo-hard-program)])
-
-  (benchmark-suite "four-fours"
-    ["4" (four-fours 4)]
-    ["12-check" (four-fours-at-12-check)]
-    ["12" (four-fours 12)]
-    ["256" (four-fours 256)])
-
-  (benchmark-suite "simple interp"
-    ["((\\x x) (\\y y))" (run 1 (q) (simple:evalo `((lambda (x) x) (lambda (y) y)) q))]
-    ["complex-countdown" (run 1 (q) (simple:evalo complex-countdown q))])
-
-  (benchmark-suite "full interp"
-    ["((\\x x) (\\y y))" (run 1 (q) (full:evalo `((lambda (x) x) (lambda (y) y)) q))]
-    ["complex-countdown" (run 1 (q) (full:evalo complex-countdown q))]
-    ["6 quines" (run 6 (q) (full:evalo q q))])
-)
-
-(module+ test
-  (require rackunit)
-
-  (check-equal?
-   (logo-hard-program)
-   '((() (_.0 _.1 . _.2) (0 0 1 0 0 0 1))
-     ((1) (_.0 _.1 . _.2) (1 1 0 0 0 0 1))
-     ((0 1) (0 1 1) (0 0 1))
-     ((1 1) (1 1) (1 0 0 1 0 1))
-     ((0 0 1) (1 1) (0 0 1))
-     ((0 0 0 1) (0 1) (0 0 1))
-     ((1 0 1) (0 1) (1 1 0 1 0 1))
-     ((0 1 1) (0 1) (0 0 0 0 0 1))
-     ((1 1 1) (0 1) (1 1 0 0 1))))
-
-  (check-equal?
-   (run 1 (q) (simple:evalo '((lambda (x) x) (lambda (y) y)) q))
-   '((closure y y ())))
-  )
+(include "../common/bench.scm")
--- bench/ee-stdlib/simple-interp.rkt
+++ bench/ee-stdlib/simple-interp.rkt
@@ -1,43 +1,7 @@
 #lang racket
 
 (require "../../main.rkt")
+
 (provide evalo)
-(define-relation (evalo expr val) (eval-expro expr '() val))
 
-(define-relation
- (eval-expro expr env val)
- (conde
-  ((fresh
-    (rator rand x body env^ a)
-    (== `(,rator ,rand) expr)
-    (eval-expro rator env `(closure ,x ,body ,env^))
-    (eval-expro rand env a)
-    (eval-expro body `((,x . ,a) . ,env^) val)))
-  ((fresh
-    (x body)
-    (== `(lambda (,x) ,body) expr)
-    (symbolo x)
-    (== `(closure ,x ,body ,env) val)
-    (not-in-envo 'lambda env)))
-  ((symbolo expr) (lookupo expr env val))))
-
-(define-relation
- (not-in-envo x env)
- (conde
-  ((== '() env))
-  ((fresh
-    (y v rest)
-    (== `((,y . ,v) . ,rest) env)
-    (=/= y x)
-    (not-in-envo x rest)))))
-
-(define-relation
- (lookupo x env t)
- (conde
-  ((fresh (y v rest) (== `((,y . ,v) . ,rest) env) (== y x) (== v t)))
-  ((fresh
-    (y v rest)
-    (== `((,y . ,v) . ,rest) env)
-    (=/= y x)
-    (lookupo x rest t)))))
-
+(include "../../mk/simple-interp.scm")
