1856b952c25db0a25e2e7c80f65e73f771623af3
Michael Ballantyne <michael.ballantyne@gmail.com>
2022-11-18 22:15:00 -0500

    don't run tests on the mk/ subfolder

--- info.rkt
+++ info.rkt
@@ -7,5 +7,7 @@
                "threading"
                ["ee-lib" #:version "1.0"]))
 
+(define test-omit-paths '("mk"))
+
 ;(define build-deps '("racket-doc" "scribble-lib"))
 ;(define scribblings '(("main.scrbl" ())))
