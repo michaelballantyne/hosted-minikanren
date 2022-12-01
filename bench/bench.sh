26bd14fbaacc85a0c125046140d7691f40c9dcb4
Michael Ballantyne <michael.ballantyne@gmail.com>
2022-12-01 09:59:03 -0500

    move bench scripts into folder

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,75 +1,75 @@
 echo "build master branch"
 git checkout master
-raco make bench/base/bench.rkt
-raco make bench/ee/bench.rkt
+raco make base/bench.rkt
+raco make ee/bench.rkt
 
 sleep 100
 
 echo "master branch base 1"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "master branch base 2"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "master branch base 3"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "master branch ee 1"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 sleep 100
 
 echo "master branch ee 2"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 sleep 100
 
 echo "master branch ee 3"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 sleep 100
 
 
 echo "build opt branch"
 git checkout opt
-raco make bench/base/bench.rkt
-raco make bench/ee/bench.rkt
+raco make base/bench.rkt
+raco make ee/bench.rkt
 
 sleep 100
 
 
 echo "opt branch base 1"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "opt branch base 2"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "opt branch base 3"
-racket bench/base/bench.rkt
+racket base/bench.rkt
 
 sleep 100
 
 echo "opt branch ee 1"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 sleep 100
 
 echo "opt branch ee 2"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 sleep 100
 
 echo "opt branch ee 3"
-racket bench/ee/bench.rkt
+racket ee/bench.rkt
 
 
