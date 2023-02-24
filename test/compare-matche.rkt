4e2f8b6485fd3dec0066df1a2f3859fccad800e1
Jason Hemann <jason.hemann@gmail.com>
2023-02-23 19:58:09 -0500

    Adding the compare matche program b/c we liked that part

---
+++ test/compare-matche.rkt
@@ -0,0 +1,88 @@
+#lang racket
+(require "../main.rkt")
+
+(defrel (hand-optimized-evalo expr val)
+  (hand-optimized-eval-expro expr '() '() val))
+
+(defrel (hand-optimized-eval-expro expr vars vals val)
+  (fresh ()
+    (conde
+     ((fresh (rator rand)
+       (== (cons rator (cons rand '())) expr)
+       (fresh (x body vars^ vals^ a)
+         (hand-optimized-eval-expro rator vars vals (cons 'closure (cons x (cons body (cons vars^ (cons vals '()))))))
+         (hand-optimized-eval-expro rand vars vals a)
+         (hand-optimized-eval-expro body (cons x vars^) (cons a vals^) val))))
+     ((fresh (x body)
+       (== (cons (cons 'lambda (cons (cons x '()) (cons body '())))
+                 (cons 'closure (cons x (cons body (cons vars (cons vals '()))))))
+           (cons expr val))
+       (symbolo x)
+       (absento 'lambda vars)))
+     ((fresh ()
+       (symbolo expr)
+       (lookupo2 expr vars vals val))))))
+
+(defrel (lookupo2 x vars vals val)
+  (fresh ()
+   (conj
+    (fresh (y vars^ v vals^)
+     (conj
+      (== (cons (cons y vars^) (cons (cons v vals^) '())) (cons vars (cons vals '())))
+      (conde
+       ((conj
+		(== y x)
+		(== v val)))
+       ((conj
+		(=/= y x)
+		(lookupo2 x vars^ vals^ val)))))))))
+
+
+(define (unoptimized-matche-interp)
+  (let ((ans (run 1000 (p q) (unoptimized-evalo p q))))
+    (list* (first ans) '... (last-pair ans))))
+
+(defrel (unoptimized-evalo expr val)
+  (unoptimized-eval-expro expr '() '() val))
+
+(defrel (unoptimized-eval-expro expr vars vals val)
+  (fresh (ls)
+    (== ls (cons expr (cons vars (cons vals (cons val '())))))
+    (conde
+     ((fresh (rator rand _1 _2 _3)
+       (== (cons (cons rator (cons rand '())) (cons _1 (cons _2 (cons _3 '())))) ls)
+       (fresh (x body vars^ vals^ a)
+         (unoptimized-eval-expro rator vars vals (cons 'closure (cons x (cons body (cons vars^ (cons vals '()))))))
+         (unoptimized-eval-expro rand vars vals a)
+         (unoptimized-eval-expro body (cons x vars^) (cons a vals^) val))))
+     ((fresh (x body vars1 vals1)
+       (== (cons (cons 'lambda (cons (cons x '()) (cons body '())))
+                 (cons vars1 (cons vals1 (cons (cons 'closure (cons x (cons body (cons vars1 (cons vals1 '()))))) '()))))
+           ls)
+       (symbolo x)
+       (absento 'lambda vars)))
+     ((fresh (_1 _2 _3 _4)
+       (== (cons _1 (cons _2 (cons _3 (cons _4 '())))) ls)
+       (symbolo expr)
+       (lookupo expr vars vals val))))))
+
+(defrel (lookupo x vars vals val)
+  (fresh (ls)
+   (conj
+    (== ls (cons vars (cons vals '())))
+    (fresh (y vars^ v vals^)
+     (conj
+      (== (cons (cons y vars^) (cons (cons v vals^) '())) ls)
+      (conde
+       ((conj
+		(== y x)
+		(== v val)))
+       ((conj
+		(=/= y x)
+		(lookupo x vars^ vals^ val)))))))))
+
+;; (pretty-print (syntax->datum (relation-code unoptimized-eval-expro)))
+;; (pretty-print (syntax->datum (relation-code/optimized unoptimized-eval-expro)))
+
+;; (pretty-print (syntax->datum (relation-code hand-optimized-eval-expro)))
+;; (pretty-print (syntax->datum (relation-code/optimized hand-optimized-eval-expro)))
