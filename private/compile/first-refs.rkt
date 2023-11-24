bd9d278bd00a163f0f078684c9645faef61c76b7
Jason Hemann <jason.hemann@gmail.com>
2023-11-24 11:14:21 -0500

    First ref works now w/goal-from-expression

--- private/compile/first-refs.rkt
+++ private/compile/first-refs.rkt
@@ -12,11 +12,15 @@
 
 (define (first-refs/entry g fvs fvs-fresh?)
   (let*-values ([(id-refs) (if fvs-fresh?
-                             (immutable-free-id-set)
-                             (immutable-free-id-set fvs))]
+                             (immutable-free-id-set fvs)
+                             (immutable-free-id-set))]
                 [(g^ _) (annotate-goal g id-refs)])
     g^))
 
+;; Takes a goal and the vars to which we have not referred, and returns
+;; an annotated goal and the new updated set of vars to which we have still not referred after
+;;
+;; Goal FIdSet -> (values Goal FIdSet)
 (define (annotate-goal g id-refs)
   (syntax-parse g #:literal-sets (mk-literals)
     [c:primitive-goal (values this-syntax id-refs)]
@@ -34,9 +38,9 @@
     [(disj g1 g2)
      (let-values ([(g1^ fst-refs) (annotate-goal #'g1 id-refs)]
                   [(g2^ snd-refs) (annotate-goal #'g2 id-refs)])
-       (values #`(disj #,g1^ #,g2^) (free-id-set-union fst-refs snd-refs)))]
+       (values #`(disj #,g1^ #,g2^) (free-id-set-intersect fst-refs snd-refs)))]
     [(fresh (x ...) g)
-     (let-values ([(g^ refs^) (annotate-goal #'g id-refs)])
+     (let-values ([(g^ refs^) (annotate-goal #'g (free-id-set-union id-refs (immutable-free-id-set (attribute x))))])
        (values #`(fresh (x ...) #,g^) refs^))]
     [(#%rel-app n t ...)
      (define-values (rev-ts refs^)
@@ -46,6 +50,8 @@
          (let-values ([(t^ refs^) (annotate-term t refs)])
            (values (cons t^ done) refs^))))
      (values #`(#%rel-app n #,@(reverse rev-ts)) refs^)]
+    [(goal-from-expression e)
+     (values this-syntax (immutable-free-id-set))]
     [(apply-relation e t ...)
      (define-values (rev-ts refs^)
        (for/fold ([done '()]
@@ -55,6 +61,12 @@
            (values (cons t^ done) refs^))))
      (values #`(apply-relation e #,@(reverse rev-ts)) refs^)]))
 
+;; Given a mK term and the set of logic variable to which we have not
+;; referred yet, return an updated term (possibly annotated
+;; w/FIRST-REF), and an updated set of variables, removing the ones we
+;; first refer to in /THIS/ term.
+;;
+;; Term FIdSet -> (Values Term FIdSet)
 (define (annotate-term t id-refs)
   (syntax-parse t
     #:literal-sets (mk-literals)
@@ -63,8 +75,8 @@
     [(rkt-term e) (values this-syntax id-refs)]
     [(#%lv-ref v)
      (if (free-id-set-member? id-refs #'v)
-       (values this-syntax id-refs)
-       (values (syntax-property this-syntax FIRST-REF #t) (free-id-set-add id-refs #'v)))]
+       (values (syntax-property this-syntax FIRST-REF #t) (free-id-set-remove id-refs #'v))
+       (values this-syntax id-refs))]
     [(cons t1 t2)
      (let*-values ([(t1^ refs^) (annotate-term #'t1 id-refs)]
                    [(t2^ refs^^) (annotate-term #'t2 refs^)])
@@ -83,8 +95,8 @@
 (begin-for-syntax
   (define (first-refs/rel stx)
     (syntax-parse stx #:literal-sets (mk-literals)
-                  [(ir-rel (x ...) g)
-                   #`(ir-rel (x ...) #,(first-refs/entry #'g (attribute x) #f))])))
+      [(ir-rel (x ...) g)
+       #`(ir-rel (x ...) #,(first-refs/entry #'g (attribute x) #f))])))
 
   (progs-equal?
     (first-refs/rel
@@ -104,7 +116,22 @@
     (generate-prog
       (ir-rel ((~binder q))
         (fresh ((~binder x))
-              (== (~missing (#%lv-ref q) FIRST-REF) (~check (#%lv-ref x) FIRST-REF))))))
+          (== (~missing (#%lv-ref q) FIRST-REF) (~check (#%lv-ref x) FIRST-REF))))))
+
+  (progs-equal?
+    (first-refs/rel
+      (generate-prog
+        (ir-rel ((~binder q))
+          (fresh ((~binder x))
+            (conj
+             (goal-from-expression #t)
+             (== (#%lv-ref q) (#%lv-ref x)))))))
+    (generate-prog
+      (ir-rel ((~binder q))
+        (fresh ((~binder x))
+          (conj
+           (goal-from-expression #t)
+           (== (~missing (#%lv-ref q) FIRST-REF) (~missing (#%lv-ref x) FIRST-REF)))))))
 
   (progs-equal?
     (first-refs/rel
