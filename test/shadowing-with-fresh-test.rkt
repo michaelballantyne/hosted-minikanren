996693d07f49e0c06674b4dd2f23e3f2c7946c1a
Michael Ballantyne <michael.ballantyne@gmail.com>
2024-05-07 14:03:01 -0400

    check in broken occurs check bug

--- test/shadowing-with-fresh-test.rkt
+++ test/shadowing-with-fresh-test.rkt
@@ -12,6 +12,13 @@
       (== ls m)   ;; ls bound, m unbound, so bind m -> ls
       (uh-o m)))) ;; so now inlining chases m -> ls -> (list m) -> (list ls) -> (list (list m)) -> ...
 
+;; An unrelated bug---constant folding doesn't do the occurs check for its compile time subst.
+(defrel (r-occurs b)
+  (fresh (x y z)
+    (== x (list y))
+    (== y x) ;; skipping occurs check here creates a cycle
+    (r2 x)))
+
 ;; This caused a runtime infinite loop due to a cyclic term, created because of an
 ;; improperly skipped occurs check.
 (defrel (r y)
@@ -21,7 +28,8 @@
       (== x 2)) ;; trick the occurs check pass into thinking x holds a constant
     (== y x) ;; skipping occurs check here creates a cycle
     (r2 x))) ;; pass term with cycle into r2 to diverge in unification
-(defrel (r2 x)
+(defrel (r2 c)
+  (== c 2)
   (fresh (z)
     (== x (list z))))
 (run 1 (q) (r q))
