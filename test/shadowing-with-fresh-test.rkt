848340dca1ba75cc60bdf7fb8c8334948530af83
Michael Ballantyne <michael.ballantyne@gmail.com>
2024-05-07 13:52:40 -0400

    broken test for shadowing in occurs check removal

--- test/shadowing-with-fresh-test.rkt
+++ test/shadowing-with-fresh-test.rkt
@@ -1,11 +1,28 @@
 #lang racket
 (require minikanren-ee)
 
-;; A regression test. Previously we had compiler bugs in the presence of shadowing.
+;; Regresssion tests for problems caused by shadowing, in case the identifier
+;; tables used in various compiler passes fail to distinguish bindings of the same name.
 
+;; This caused a compile-time infinite loop in constant folding.
 (defrel (uh-o ls)
   (fresh (m)
-    (== ls `(,m))
+    (== ls `(,m)) ;; substitution maps ls -> (list m)
     (fresh (ls)
-      (== ls m)
-      (uh-o m))))
+      (== ls m)   ;; ls bound, m unbound, so bind m -> ls
+      (uh-o m)))) ;; so now inlining chases m -> ls -> (list m) -> (list ls) -> (list (list m)) -> ...
+
+;; This caused a runtime infinite loop due to a cyclic term, created because of an
+;; improperly skipped occurs check.
+(defrel (r y)
+  (fresh (x y z)
+    (== x (list y))
+    (fresh (x)
+      (== x 2)) ;; trick the occurs check pass into thinking x holds a constant
+    (== y x) ;; skipping occurs check here creates a cycle
+    (r2 x))) ;; pass term with cycle into r2 to diverge in unification
+(defrel (r2 x)
+  (fresh (z)
+    (== x (list z))))
+(run 1 (q) (r q))
+
