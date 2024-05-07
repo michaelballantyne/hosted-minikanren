619eae46798649d01e74a1e10bb9cd7a8430550e
Jason Hemann <jason.hemann@gmail.com>
2024-05-07 15:13:09 -0400

    Integration tests for bugs in fold.rkt

--- test/shadowing-with-fresh-test.rkt
+++ test/shadowing-with-fresh-test.rkt
@@ -1,5 +1,7 @@
-#lang racket
-(require minikanren-ee)
+#lang racket/base
+
+(require (except-in rackunit fail)
+         "../main.rkt")
 
 ;; Regresssion tests for problems caused by shadowing, in case the identifier
 ;; tables used in various compiler passes fail to distinguish bindings of the same name.
@@ -12,13 +14,6 @@
       (== ls m)   ;; ls bound, m unbound, so bind m -> ls
       (uh-o m)))) ;; so now inlining chases m -> ls -> (list m) -> (list ls) -> (list (list m)) -> ...
 
-;; An unrelated bug---constant folding doesn't do the occurs check for its compile time subst.
-(defrel (r-occurs b)
-  (fresh (x y z)
-    (== x (list y))
-    (== y x) ;; skipping occurs check here creates a cycle
-    (r2 x)))
-
 ;; This caused a runtime infinite loop due to a cyclic term, created because of an
 ;; improperly skipped occurs check.
 (defrel (r y)
@@ -29,8 +24,18 @@
     (== y x) ;; skipping occurs check here creates a cycle
     (r2 x))) ;; pass term with cycle into r2 to diverge in unification
 (defrel (r2 c)
-  (== c 2)
   (fresh (z)
-    (== x (list z))))
-(run 1 (q) (r q))
+    (== c (list z))))
+(check-equal?
+ (run 1 (q) (r q))
+ '())
 
+(define-syntax m
+  (goal-macro
+    (syntax-rules ()
+      [(_ a b)
+       (fresh (x)
+         (== a x)
+         (== b x))])))
+
+(run 1 (x) (== x 1) (m x))
