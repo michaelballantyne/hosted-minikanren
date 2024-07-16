2be9573e4cc703da7cfa91f406d7611a8a27cff1
Michael Ballantyne <michael.ballantyne@gmail.com>
2024-07-16 15:00:19 -0400

    fix up demo compiler tests

--- demos/icfp2024/demo-compiler-tests.rkt
+++ demos/icfp2024/demo-compiler-tests.rkt
@@ -1,21 +1,32 @@
 #lang racket
 (require (except-in rackunit fail))
-(require "./minikanren-spec-compiler.rkt")
+(require "./demo-compiler.rkt")
 
 (check-equal?
  (run 1 (x) (== x x))
  '(_.0))
 
 (check-equal?
- (run 1 (x) (== x (quote cat)))
- '(cat))
-
-(check-equal?
  (run 1 (q) (== q 'cat))
  '(cat))
 
+(defrel (foo x y z)
+  (== x y))
+
+(check-equal?
+ (run 1 (p q r) (foo p q r))
+ '((_.0 _.0 _.1)))
+
+(check-equal? 
+ (run 1 (q) (absento 'cat q))
+ '((_.0 (absento (cat _.0)))))
+
 (check-equal?
  (run 1 (q) (== q (term-from-expression 'cat)))
+ '(cat))
+
+(check-equal?
+ (run 1 (q) (== q (term-from-expression (expression-from-term 'cat))))
  '(cat))
 
 (check-equal?
@@ -28,14 +39,19 @@
  '(_.0))
 
 (check-equal?
- (run 1 (q) (== q (term-from-expression (expression-from-term 'cat))))
- '(cat))
-
-(check-equal?
  (run 1 (q) (goal-from-expression (expression-from-goal succeed)))
  '(_.0))
 
-(check-equal?
+(test-exn
+ "goal-from-expression only accepts sealed goal values produced by expression-from-goal"
+ #rx"expected: mk-goal?"
+ (λ ()
+   (run 1 (q)
+     (goal-from-expression
+      (λ (st) st)))))
+
+(test-equal?
+ "expression-from-goal produces sealed goal values"
  (with-output-to-string
    (λ ()
      (displayln
@@ -46,14 +62,8 @@
            (expression-from-goal succeed)))))))
  "this is a goal: #<mk-goal>:\n(_.0)\n")
 
-(check-exn
- #rx"mk-goal?"
- (λ ()
-   (run 1 (q)
-     (goal-from-expression
-      (λ (st) st)))))
-
-(check-equal?
+(test-equal?
+ "expression-from-term translates term values and seals logic variables"
  (with-output-to-string
    (λ ()
      (displayln
@@ -66,14 +76,14 @@
              (expression-from-goal succeed))))))))
  "q is (#<mk-lvar> #<mk-lvar>):\n((_.0 _.0))\n")
 
-(test-equal? "list macro expands correctly"
+(test-equal? "The list macro expands correctly"
  (test-goal-syntax (fresh (y) (absento (list 'cat 'cat 'cat) y)))
  '(fresh1 (y)
     (conj (absento
-       (cons
-        (core-quote cat)
-        (cons (core-quote cat) (cons (core-quote cat) (core-quote ()))))
-       y))))
+            (cons
+             (core-quote cat)
+             (cons (core-quote cat) (cons (core-quote cat) (core-quote ()))))
+            y))))
 
 (test-equal? "Quasiquote and comma work"
  (test-goal-syntax (fresh (y) (absento `(,y fish) y)))
@@ -84,18 +94,6 @@
  (test-goal-syntax (fresh (x y) (== y x)))
  '(fresh1 (x y) (conj (== y x))))
 
-(defrel (foo x y z)
-  (== x y))
-
-(test-equal? "Rel-app invocation seems to work"
+(check-equal? 
  (test-goal-syntax (fresh (y) (foo 'cat 'cat y)))
  '(fresh1 (y) (conj (foo (core-quote cat) (core-quote cat) y))))
-
-;; This doesn't work the way it should.
-(test-equal? "Calling relations seems to work correctly"
- (run 1 (p q r) (foo p q r))
- '((_.0 _.0 _.1)))
-
-(test-equal? "Absento call works"
- (run 1 (q) (absento 'cat q))
- '((_.0 (absento (cat _.0)))))
