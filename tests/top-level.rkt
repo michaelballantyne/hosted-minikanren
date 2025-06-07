8900a76d13a1295eb4368d5b54ac93f534b00b00
Jason Hemann <jason.hemann@gmail.com>
2025-06-07 09:42:17 -0400

    Permit nullary relations and tests

--- tests/top-level.rkt
+++ tests/top-level.rkt
@@ -10,3 +10,28 @@
 (check-equal?
   (run 2 (q) (ones q))
   '(1 1))
+
+(defrel (bar x)
+  (== 'cat x))
+
+(defrel (call-bar)
+  (bar 'cat))
+
+(defrel (baz x)
+  (fresh (e1)
+    (== x `(,e1 fish))))
+
+(defrel (call-baz)
+  (fresh (x y)
+    (== x (list y))
+    (baz (list x y))))
+
+(test-equal?
+  "confirms that nullary relations can compile and run"
+  (run 1 (q) (call-bar))
+  '(_.0))
+
+(test-equal?
+  "confirms that nullary relations w/fresh can compile and run"
+  (run 1 (q) (call-baz))
+  '(_.0))
